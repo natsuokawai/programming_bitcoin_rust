0130909372abed67633737b0686fd763506b85fe
natsuokawai <kawai.natsuo@gmail.com>
2021-04-26 22:07:37 +0900

    Introduce point at infinity

--- point/src/lib.rs
+++ point/src/lib.rs
@@ -1,28 +1,49 @@
 use std::fmt;
 
 #[derive(PartialEq, Debug)]
+enum Coordinate {
+    Num(i32),
+    Inf,
+}
+
+impl fmt::Display for Coordinate {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                Coordinate::Num(x) => x.to_string(),
+                _ => String::from("Inf"),
+            }
+        )
+    }
+}
+
+#[derive(PartialEq, Debug)]
 struct Point {
     a: i32,
     b: i32,
-    x: i32,
-    y: i32,
+    x: Coordinate,
+    y: Coordinate,
 }
 
 impl Point {
-    fn new(x: i32, y: i32, a: i32, b: i32) -> Self {
-        let result = Point {
-            a, b, x, y
-        };
+    fn new(x: Coordinate, y: Coordinate, a: i32, b: i32) -> Self {
+        let result = Point { a, b, x, y };
 
         if !result.is_on_curve() {
-            panic!("({}, {}) is not on the curve.", x, y);
+            panic!("({}, {}) is not on the curve.", result.x, result.y);
         }
 
         result
     }
 
     fn is_on_curve(&self) -> bool {
-        self.y.pow(2) == self.x.pow(3) + self.a * self.x + self.b
+        match (&self.x, &self.y) {
+            (Coordinate::Inf, Coordinate::Inf) => true,
+            (Coordinate::Num(x), Coordinate::Num(y)) => y.pow(2) == x.pow(3) + self.a * x + self.b,
+            (_, _) => false,
+        }
     }
 }
 
@@ -42,7 +63,7 @@ mod tests {
 
     #[test]
     fn equality_test() {
-        let p = Point::new(18, 77, 5, 7);
+        let p = Point::new(Coordinate::Num(18), Coordinate::Num(77), 5, 7);
         assert_eq!(p, p);
     }
 }
